e97a38b4e40c8b73970aa98665d7ccedea72c6cc
Borrison <borissoto@gmail.com>
2020-09-11 21:51:54 -0400

    Initial Commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { IonicPage, NavController } from "ionic-angular";
+import { Observable } from "rxjs/Observable";
+import { Item } from "../../modules/item/item.module";
+import { ShoppingListService } from "../../services/shopping-list/shopping-list.service";
 
+@IonicPage()
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html",
 })
 export class HomePage {
+  shoppingList$: Observable<Item[]>;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(
+    public navCtrl: NavController,
+    private shopping: ShoppingListService
+  ) {
+    this.shoppingList$ = this.shopping
+      .getShoppingList() // DB list
+      .snapshotChanges() // Key and Value
+      .map((changes) => {
+        return changes.map((c) => ({
+          key: c.payload.key,
+          ...c.payload.val(),
+        }));
+      });
   }
-
 }
